c2c9228dc57edbf6a53808fc91aa825509a77e6b
Pavel Ershov <pavel@ershov.org>
2022-08-29 19:28:03 +0300

    sockets facelifint

--- src/pages/Websocket.js
+++ src/pages/Websocket.js
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
-import {useAuth} from "../auth";
+import { useAuth } from "../auth";
 
-const socket = io('', {autoConnect: false})
+const socket = io('', { autoConnect: false })
 
 //or you could connect without proxy
 //const socket = io('https://api.directual.com/', {autoConnect: false})
@@ -10,13 +10,14 @@ const socket = io('', {autoConnect: false})
 export default function Websocket() {
     const [isConnected, setIsConnected] = useState(socket.connected);
     const [message, setMessage] = useState(null);
+    const [allMessages, setAllMessages] = useState(null)
     const auth = useAuth();
 
     useEffect(() => {
         if (auth.isAutorised()) {
             fetch("/appid").then((response) => {
                 response.json().then(data => {
-                    socket.auth = {app_id: data.APP_ID, session_id: auth.sessionID};
+                    socket.auth = { app_id: data.APP_ID, session_id: auth.sessionID };
                     socket.connect();
                 })
             })
@@ -24,12 +25,13 @@ export default function Websocket() {
 
         //subscribe to any events
         socket.onAny((eventName, args) => {
-            setMessage(`${eventName}: ${JSON.stringify(args)}`);
+            //setMessage(`${eventName}: ${JSON.stringify(args)}`);
+            setMessage(<div>{`${eventName}: ${JSON.stringify(args)}`} &nbsp;<code>{(new Date()).toISOString()}</code></div>);
         });
 
         //subscribe to only 'message' events
         socket.on('message', (msg) => {
-            setMessage(msg);
+            setMessage(<div>{msg} &nbsp;<code>{(new Date()).toISOString()}</code></div>);
         })
 
         socket.on('connect', () => {
@@ -46,12 +48,19 @@ export default function Websocket() {
         };
     }, []);
 
+    useEffect(() => {
+        setAllMessages(<React.Fragment>{message}{allMessages}</React.Fragment>)
+    }, [message])
+
     return (
         <div className="content">
             <h1>Websocket test</h1>
             <div className="request-info">
-                <span>Is connected: {isConnected ? 'yes' : 'no'}</span>
-                <span>Last message: {!message ? '-' : message}</span>
+                <p>Is connected: <code><b>{isConnected ? 'yes' : 'no'}</b></code></p>
+                <p>Message hystory:</p>
+                <div>
+                    {!allMessages ? '—' : allMessages}
+                </div>
             </div>
         </div>
     )
